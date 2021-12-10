1b454725f16f1eb093ee88fbbb4367adb35b2bd0
Hari <you@example.com>
2021-12-10 14:16:34 +0530

    RUstlanf

--- fish/config.fish
+++ fish/config.fish
@@ -5,9 +5,6 @@ end
 
 # alias ls='exa '
 # alias ll='exa --icons --all'
-#echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/testing/.profile
-echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/testing/.profile
-eval "(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 alias et='exa -l --icons -T'
 alias e='exa -l --icons'
 alias p='python3'
