4893c538e1c18e983ae17322ba5a1ed88035d788
Your Name <you@example.com>
2021-12-17 09:08:26 +0000

    Update

--- install.sh
+++ install.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 sudo apt update 
 sudo apt upgrade
-sudo apt install build-essential fzf clangd stow lldb git htop tmux ninja-build gettext libtool ccls libtool-bin autoconf automake cmake g++ pkg-config unzip curl doxygen
+sudo apt install build-essential fzf clangd stow curl file git htop tmux ninja-build gettext libtool ccls libtool-bin autoconf automake cmake g++ pkg-config unzip curl doxygen
 ################################
 # Brew 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-brew install gcc bat fd rg the_silver_searcher tig gh fzf exa fish gdb go gopls rust-analyzer wget gdb npm node python
+echo "eval \"\$($(brew --prefix)/bin/brew shellenv)\"" >>~/.bash_profile
+echo "eval \"\$($(brew --prefix)/bin/brew shellenv)\"" >>~/.profile
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+brew install gcc lua-language-server bat fd rg the_silver_searcher tig gh fzf exa fish gdb go gopls rust-analyzer wget gdb npm node python
+git config --global core.editor "nvim"
 # Neovim
-mkdir  ~/temp
 cd ~/temp
 git clone https://github.com/neovim/neovim
 cd neovim && make
@@ -16,4 +19,9 @@ sudo make install
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | so Ruscurl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | s' --tlsv1.2 -sSf https://sh.rustup.rs | sht'
 # tmux and configs
 cd ~/
-git clone https://github.com/pynvimdev/.config
+git clone https://github.com/pynvimdev/nvim-config
+mv nvim-config nvim 
+mv nvim ~/.config/
+# Private repos
+#git clone https://github.com/pynvimdev/.config
+gh auth login
